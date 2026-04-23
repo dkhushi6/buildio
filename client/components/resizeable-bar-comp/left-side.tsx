4aca3ca5c6f8e248ff0b6106666f79d98e691575
Khushi Dhankhar <khushidhankhar006@gmail.com>
2026-04-23 22:23:40 +0530

    fix: client socket url update

--- client/components/resizeable-bar-comp/left-side.tsx
+++ client/components/resizeable-bar-comp/left-side.tsx
@@ -82,7 +82,7 @@ const LeftSide = ({
         console.log("no projectId");
       }
       const userId = session?.user?.id;
-      const socket = io(process.env.NEXT_PUBLIC_API_URL);
+      const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
       socket.emit("projectId", projectId);
       socket.emit("userId", userId);
       socket.emit("getcode", prompt);
